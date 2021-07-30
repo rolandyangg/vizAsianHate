afb455bafbc6eb703846b6632a6ee825bd340233
whyroland <42717361+whyroland@users.noreply.github.com>
2021-07-30 18:00:29 -0400

    D3

--- src/components/bubblegraph/bubblegraph.jsx
+++ src/components/bubblegraph/bubblegraph.jsx
@@ -1,5 +1,10 @@
 import React from "react";
 
+
 export default function BubbleGraph() {
 	return <></>;
+}
+
+export async function getStaticProps(context) {
+	
 }
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,11 +3,13 @@ import {
 } from "@chakra-ui/react"
 import React from "react";
 import Container from "../components/layout/container"
+import BubbleGraph from "../components/bubblegraph/bubblegraph"
 
 export default function Home() {
   return (
     <Container>
       <Text>Cheese</Text>
+      <BubbleGraph/>
     </Container>
   )
 }
