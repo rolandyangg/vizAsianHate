990131da9a450bc23e6fb4bea0a1ad4a058e4fad
ahaanlimaye <ahaan.limaye@gmail.com>
2021-07-31 14:04:36 -0400

    mapbox

--- src/components/mapbox.jsx
+++ src/components/mapbox.jsx
@@ -1,9 +1,37 @@
-import React from "react";
+import { useRef, useState, useEffect } from 'react';
+import mapboxgl from '!mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
+
+mapboxgl.accessToken = 'pk.eyJ1IjoiYWhhYW5saW1heWUiLCJhIjoiY2tycHF5eXdlMmI3NDJub2JiMWx2NGp4aiJ9.eGODl97VCEfsezbT7_ZNjA';
 
 export default function Mapbox() {
-    return (
-        <>
-        </>
-    )
-}
+  const mapContainer = useRef(null);
+  const map = useRef(null);
+  const [lng, setLng] = useState(-70.9);
+  const [lat, setLat] = useState(42.35);
+  const [zoom, setZoom] = useState(9);
 
+  useEffect(() => {
+    if (map.current) return; // initialize map only once
+    map.current = new mapboxgl.Map({
+      container: mapContainer.current,
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: [lng, lat],
+      zoom: zoom
+    });
+  }, [lng, lat, zoom]);
+
+  useEffect(() => {
+    if (!map.current) return; // wait for map to initialize
+    map.current.on('move', () => {
+      setLng(map.current.getCenter().lng.toFixed(4));
+      setLat(map.current.getCenter().lat.toFixed(4));
+      setZoom(map.current.getZoom().toFixed(2));
+    });
+  }, [lng, lat, zoom]);
+
+  return (
+    <div>
+      <div ref={mapContainer} className="map-container" style={{ height: '1000px'}} />
+    </div>
+  );
+};
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -7,7 +7,7 @@ import Container from "../components/layout/container"
 import BubbleGraph from "../components/bubblegraph/bubblegraph"
 
 export default function Home() {
-  return (
+ return (
     <Container py={20} bgColor="black">
       <Box justify="center" align="center">
         <Text>Bubble Graph</Text>
