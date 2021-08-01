4e2d9362e4e1adbbb9575e5c0a12dc1f37e813ed
whyroland <42717361+whyroland@users.noreply.github.com>
2021-08-01 07:44:04 -0400

    Timeline

--- src/components/timeline/timeline.jsx
+++ src/components/timeline/timeline.jsx
@@ -1,6 +1,17 @@
+/* eslint-disable no-unused-vars */
+import { Chrono } from "react-chrono";
+import timelineElements from "./timelinedata"
+import { Box, Text, Flex } from "@chakra-ui/react";
+
 export default function Timeline() {
     return (
         <>
+            <Flex h="700px">
+                <Chrono items={timelineElements}
+                    slideShow 
+                    mode="VERTICAL_ALTERNATING"
+                />
+            </Flex>
         </>
     );
 }
---
+++ src/components/timeline/timelinedata.js
@@ -0,0 +1,151 @@
+const timelineElements = [
+    {
+      cardTitle: "New Law Prohibits Travelers from entering the U.S to Reduce Risk of Coronavirus.",
+      cardSubtitle: "New Covid Cases in U.S (Daily): 0",
+      cardDetailedText: 
+        "President Trump signed the “Proclamation on Suspension of Entry as Immigrants and Nonimmigrants of Persons who Pose a Risk of Transmitting 2019 Novel Coronavirus.” This banned almost all foreign nationals from entering the U.S. if they had been in China during any of the 14 days prior to their arrival to the U.S.",
+      title: "January 31, 2020",
+    },
+    {
+        cardTitle: "Limitation on Number of Chinese Working Citizens.",
+        cardSubtitle: "New Covid Cases in U.S (Daily): 840",
+        cardDetailedText: 
+          "The Trump administration limited to 100 (collectively) the number of Chinese citizens who may work in the United States for five state-controlled Chinese news organizations, effective March 13.",
+        title: "March 13, 2020",
+      },
+      {
+        cardTitle: "Protecting Our Pharmaceutical Supply Chain from China Act of 2020.",
+        cardSubtitle: "New Covid Cases in U.S (Daily): 4,654",
+        cardDetailedText: 
+          "Sen. Tom Cotton (R-AR) introduced the “Protecting Our Pharmaceutical Supply Chain from China Act of 2020.” This bill prohibits pharmaceutical purchases from China or products with active pharmaceutical ingredients created in China. (Rep. Mike Gallagher (R-WI) 7 introduced this to the House on April 10.)",
+        title: "March 19, 2020",
+      },
+      {
+        cardTitle: "New Resolution Condemns China for Spreading of Coronavirus.",
+        cardSubtitle: "New Covid Cases in U.S (Daily): 8,801",
+        cardDetailedText: 
+          "Rep. Jim Banks (R-IN) introduced the resolution, “Expressing the sense of the House of Representatives that the government of the People's Republic of China made multiple, serious mistakes in the early stages of the COVID-19 outbreak that heightened the severity and spread of the ongoing COVID-19 pandemic, which include the Chinese government's intentional spread of misinformation to downplay the risks of the virus, a refusal to cooperate with international health authorities, internal censorship of doctors and journalists and malicious disregard for the health of ethnic minorities.” This resolution condemns China for its handling of the COVID-19 pandemic.",
+        title: "March 24, 2020",
+      },
+      {
+        cardTitle: "Introduction of Chines Governemnt COVID-19 Accountability Act.",
+        cardSubtitle: "New Covid Cases in U.S (Daily): 17,608",
+        cardDetailedText: 
+          "Rep. Gregory Steube (R-FL) introduced the “Chinese Government COVID-19 Accountability Act.” This bill directs the president to develop and carry out a strategy to get China's government to reimburse the U.S. government for funds made available to address COVID-19.",
+        title: "March 26, 2020",
+      },
+      {
+        cardTitle: "Proposal for closing all Wildlife Markets and stopping all Misinformation about Coronavirus.",
+        cardSubtitle: "New Covid Cases in U.S (Daily): 25,866",
+        cardDetailedText: 
+          "Rep. Steve Chabot (R-OH) introduced the resolution, “Expressing the sense of the House of Representatives that all nations should permanently close live wildlife markets and that the People's Republic of China should cease spreading disinformation regarding the originsof coronavirus.”",
+        title: "April 14, 2020",
+      },
+      {
+        cardTitle: "Introduction of New Bill holding China Accountable for Effects of Coronavirus.",
+        cardSubtitle: "New Covid Cases in U.S (Daily): 27,950",
+        cardDetailedText: 
+          "Rep. Dan Crenshaw (R-TX) introduced “Holding the Chinese Communist Party Accountable for Infecting Americans Act of 2020.” This bill allows Americans to sue China in federal court to” recover damages for death, injury and economic harm” caused by COVID-19.",
+        title: "April 17, 2020",
+      },
+      {
+        cardTitle: "Implimitation of new Proclamation Limiting Immigrants who pose a Risk to the U.S. Market.",
+        cardSubtitle: "New Covid Cases in U.S (Daily): 25,649",
+        cardDetailedText: 
+          "President Trump signed the “Proclamation Suspension of Entry of Immigrants Who Present a Risk to the United States Labor Market During the Economic Recovery Following the 2019 Novel Coronavirus Outbreak” This suspended immigration into the U.S. for 60 days if foreign nationals are outside the U.S., do not have a valid visa, and do not have another official travel document",
+        title: "April 28, 2020",
+      },
+      {
+        cardTitle: "New Bill Blames China for Medical Issues arrising in the U.S.",
+        cardSubtitle: "New Covid Cases in U.S (Daily): 32,809",
+        cardDetailedText: 
+          "Rep. Chip Roy (R-TX) introduced the “Bring Entrepreneurial Advancements To Consumers Here In North America Act (BEAT CHINA).” This bill blames China for the COVID-19 pandemic and incentivizes pharmaceutical and medical device and supply manufacturers to relocate to the United States.",
+        title: "May 1, 2020",
+      },
+      {
+        cardTitle: "Proposal of Bill that makes China Responsible for Economic Downfall of U.S.",
+        cardSubtitle: "New Covid Cases in U.S (Daily): 25,789",
+        cardDetailedText: 
+          "Sen. Marsha Blackburn (R-TN) introduced the “Stop China-Originated Viral Infectious Diseases (COVID) Act.” This bill makes China legally and financially liable for the COVID-19 pandemic in the United States.",
+        title: "May 4, 2020",
+      },
+      {
+        cardTitle: "Introduction of New Bill that a Coronavirus Task Force will be Established.",
+        cardSubtitle: "New Covid Cases in U.S (Daily): 17,939",
+        cardDetailedText: 
+          "Sen. Josh Hawley (R-MO) introduced the “Justice for Victims of Coronavirus Around the World Act.” This bill stipulates that the president would establish a Justice for Victims of Coronavirus Task Force to investigate and secure damages from China.",
+        title: "May 11, 2020",
+      },
+      {
+        cardTitle: "Introduction of “COVID-19 Acountability Act”.",
+        cardSubtitle: "New Covid Cases in U.S (Daily): 17,939",
+        cardDetailedText: 
+          "Sen. Lindsey Graham (R-SC) introduced the “COVID-19 Accountability Act.” This bill authorizes sanctions in relation to the Chinese government’s response to COVID-19. It requires the president to certify that the Chinese government (1) is cooperating with efforts related to the COVID-19 outbreak, (2) has prohibited wet markets and (3) has released and dropped all charges for anyone involved in protests in Hong Kong related to COVID-19. If the president cannot make such a certification, the president may (1) impose sanctions on Chinese government officials, entities owned or controlled by the Chinese government, and individuals affiliated with such entities; or (2) prohibit Chinese nationals from obtaining student visas (i.e., F and M visas) or exchange visitor visas (i.e., J visas)(Rep. Doug Collins (R-GA) introduced this to the House on May 14.)",
+        title: "May 11, 2020",
+      },
+      {
+        cardTitle: "Inception of the “Compensation for Americans Act of 2020”.",
+        cardSubtitle: "New Covid Cases in U.S (Daily): 24,237",
+        cardDetailedText: 
+          "Rep. Ann Wagner (R-MO) introduced the “Compensation for Americans Act of 2020.” This bill requires the U.S. Department of State to negotiate with China to secure compensation for damages due to COVID-19, and allows the president to impose sanctions on Chinese nationals and corporations, freeze Chinese assets, and prohibit using federal funds to procure goods manufactured or sourced from a Chinese company. It also requires U.S. World Bank representatives to end China’s eligibility for World Bank assistance.",
+        title: "May 22, 2020",
+      },
+      {
+        cardTitle: "New Bill prohibits Chinese Nationals from Receiving Visas to U.S..",
+        cardSubtitle: "New Covid Cases in U.S (Daily): 20,996",
+        cardDetailedText: 
+          "Rep. David Kustoff (R-TN) introduced the “SECURE CAMPUS Act of 2020.” This bill prohibits Chinese nationals from receiving visas to the United States for graduate or postgraduate studies in STEM fields. (Sen. Tom Cotton (R-AR) introduced this to the Senate on June 9.)",
+        title: "May 27, 2020",
+      },
+      {
+        cardTitle: "Suspension of Chinese Nationals from Allowing to Enter the U.S. with only Visa.",
+        cardSubtitle: "New Covid Cases in U.S (Daily): 23,621",
+        cardDetailedText: 
+          "President Trump signed the “Proclamation on the Suspension of Entry as Nonimmigrants of Certain Students and Researchers from the People’s Republic of China.” This suspended the entry of certain Chinese nationals seeking to enter the United States on an F or J visa (i.e., student visas) to study or conduct research in the United States.",
+        title: "May 29, 2020",
+      },
+      {
+        cardTitle: "Prohibition of allowing Chinese to enter America for Reasons under STEM.",
+        cardSubtitle: "New Covid Cases in U.S (Daily): 22,144",
+        cardDetailedText: 
+          "Rep. Devin Nunes (R-CA) introduced the “Holding China Accountable Act.” This bill prohibits travel to the United States by Chinese nationals whose visit involves science, technology, engineering, mathematics or a related field.",
+        title: "June 11, 2020",
+      },
+      {
+        cardTitle: "Implimitation of Preventing Aliens entering the U.S. who Pose a Risk.",
+        cardSubtitle: "New Covid Cases in U.S (Daily): 33,465",
+        cardDetailedText: 
+          "President Trump signed the “Proclamation Suspending Entry of Aliens Who Present a Risk to the U.S. Labor Market Following the Coronavirus Outbreak.” This expanded and extended his April 22 proclamation by extending the initial green card ban until December 31, 2020, and includes additional significant restrictions on several categories of temporary guest worker visas, including H1B visas.",
+        title: "June 22, 2020",
+      },
+      {
+        cardTitle: "Introduction of “Civil Justice for Victims of COVID Act”.",
+        cardSubtitle: "New Covid Cases in U.S (Daily): 64,230",
+        cardDetailedText: 
+          "Sen. Martha McSally (R-AZ) introduced the “Civil Justice for Victims of COVID Act.” This bill allows Americans to sue China in federal court in relation to COVID-19.",
+        title: "July 20, 2020",
+      },
+      {
+        cardTitle: "Banned transactions between U.S. Citizens and WeChat.",
+        cardSubtitle: "New Covid Cases in U.S (Daily): 60,828",
+        cardDetailedText: 
+          "President Trump signed the “Executive Order on Addressing the Threat Posed by WeChat.” This banned transactions between U.S. nationals and the parent company of WeChat, Tencent Holdings, LTD.",
+        title: "August 6, 2020",
+      },
+      {
+        cardTitle: "Start of the Government not being able to Host Racial Sensitivity Training.",
+        cardSubtitle: "New Covid Cases in U.S (Daily): 48,985",
+        cardDetailedText: 
+          "President Trump signed the “Executive Order on Combating Race and Sex Stereotyping.” This bans the federal government and its contractors from hosting racial sensitivity training.",
+        title: "September 22, 2020",
+      },
+      {
+        cardTitle: "States and Cities are Needed to give Consent for Resettling Refugees.",
+        cardSubtitle: "New Covid Cases in U.S (Daily): 55,240",
+        cardDetailedText: 
+          "President Trump signed the “Executive Order on Enhancing State and Local Involvement in Refugee Resettlement.” This requires the consent of states and cities to resettle refugees in their jurisdictions and reduces the number of refugees to be resettled to the United States from 110,000 in the last year of the Obama administration to only 18,000.",
+        title: "September 26, 2020",
+      },
+  ];
+  
+  export default timelineElements;
