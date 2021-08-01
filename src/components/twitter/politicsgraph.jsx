1206b9262f06d8d1b0f1ab8214c824e0e6ae5733
whyroland <42717361+whyroland@users.noreply.github.com>
2021-08-01 12:14:52 -0400

    Politics Description

--- src/components/twitter/politicsgraph.jsx
+++ src/components/twitter/politicsgraph.jsx
@@ -1,16 +1,13 @@
-import { Box, Text, Heading, Flex } from "@chakra-ui/react"
+import { Text, Heading, Flex, VStack } from "@chakra-ui/react"
 import {
 	BarChart,
 	Bar,
-	Cell,
 	XAxis,
 	YAxis,
 	CartesianGrid,
 	Tooltip,
 	Legend,
-	ResponsiveContainer
 } from 'recharts';
-import { PureComponent } from 'react';
 
 import PoliticsTweetData from "./asianhatetweetdata"
 
@@ -19,7 +16,7 @@ export default function PoliticsGraph() {
 		<Flex align="center" justify="center">
 			<BarChart
 			layout="vertical"
-			width={700}
+			width={600}
 			height={450}
 			data={PoliticsTweetData}
 			margin={{
@@ -28,6 +25,7 @@ export default function PoliticsGraph() {
 				left: 20,
 				bottom: 5
 			}}
+			mx={15}
 			>
 				<CartesianGrid stroke="#ccc" strokeDasharray="3 3" />
 				<XAxis dataKey="# of Tweets" type="number"/>
@@ -37,9 +35,18 @@ export default function PoliticsGraph() {
 				<Bar dataKey="Democrats" stackId="a" fill="#E49B0F" />
 				<Bar dataKey="Republicans" stackId="a" fill="#E4D00A" />
 			</BarChart>
-			<Heading>
-				Test
-			</Heading>
+			<VStack mx={10}>
+				<Heading fontSize={30}>
+					"Tweets deploying stigmatizing rhetoric were retweeted a total of 1,310,828 times and like a total of 4,488,173 times"
+				</Heading>
+				<Text fontSize={17}>
+					Online as well, Social Media proved to be a platform for hate to proliferate.
+					In the first eight months of the COVID-19 pandemic, researchers examined the tweets
+					issued by both Republican and Democrat general election candidates for the offices of President, Vice President, and U.S. Senator.
+					Researchers searched for tweets that contained any terms relating to AAPI from their accounts, and
+					analyzed their rhetoric and intent.
+				</Text>
+			</VStack>
 		</Flex>
     );
 }
