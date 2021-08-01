6e26d1b54dc33215e4bf3944a580a95325ee262d
fanman1 <59294394+fanman1@users.noreply.github.com>
2021-08-01 12:18:08 -0400

    Update index.jsx

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -33,7 +33,7 @@ export default function Home() {
           <Flex align="center" justify="center" flexWrap="wrap">
             <Box>
               <MainGraph justify="center" alsign="center"/>
-              <Text justify="left">This graph shows the correlation between anti-Asian hate crimes and COVID-19 infection rates in California between January 2019 to September 2020. The red line intersecting March 2020 indicated when the lockdown mandates began taking place around the United States. In March 2020, infection rates significantly increased and by the end of the month over 8000 people were infected. At the time, people began to panic and buy extra groceries, fight over hand sanitizer, and, most unfortunately, pin the blame of the virus on Asians. As seen in California, hate crime reports shot up from 4 to 17 once COVID cases began to rise. That is an absurd increase of 325% in 1 month! Over time as cases grew, however, the line of hate crimes is seen decreasing. 
+              <Text align="left" justyfi="left" margin="30">This graph shows the correlation between anti-Asian hate crimes and COVID-19 infection rates in California between January 2019 to September 2020. The red line intersecting March 2020 indicated when the lockdown mandates began taking place around the United States. In March 2020, infection rates significantly increased and by the end of the month over 8000 people were infected. At the time, people began to panic and buy extra groceries, fight over hand sanitizer, and, most unfortunately, pin the blame of the virus on Asians. As seen in California, hate crime reports shot up from 4 to 17 once COVID cases began to rise. That is an absurd increase of 325% in 1 month! Over time as cases grew, however, the line of hate crimes is seen decreasing. 
               
               </Text>
               <Text>
@@ -42,7 +42,7 @@ export default function Home() {
               Not all hate crimes are reported to the police due to limited English proficiency and the inability to report a hate crime in their primary language or a distrust of the police. Therefore, the real numbers may not be reflected in our data.
               As the lockdown progressed over time, less people were outside and less hate crimes were reported as a result.
               </Text>
-              <Text>
+              <Text margin="30">
                 Nonetheless, the increase is unignorable and the issue has become a deep issue in America. Victims of hate crimes often do not report their victimization to local LEAs, resulting in the underreporting of hate crimes. This underreporting impacts the hate crime data received by the Department of Justice, resulting in an incomplete picture of the hate crimes committed within the state each year. It is important for victims of hate crimes to report their victimization to their local police.
 
               </Text>
