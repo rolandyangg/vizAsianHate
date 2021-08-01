ce50794a3dc317b9d2d1aea8ae0177cddff99936
whyroland <42717361+whyroland@users.noreply.github.com>
2021-07-31 21:44:57 -0400

    Organized Exports Imports

---
+++ src/components/index.jsx
@@ -0,0 +1,9 @@
+import AdditionGraph from "./bubblegraph/additiongraph"
+import BubbleGraph from "./bubblegraph/bubblegraph"
+import MainGraph from "./regulargraphs/maingraphs"
+import PoliticsGraph from "./twitter/politicsgraph"
+import LiveTwitter from "./twitter/livedata"
+import Hero from "./hero"
+import Mapbox from "./mapbox"
+
+export { AdditionGraph, BubbleGraph, MainGraph, PoliticsGraph, LiveTwitter, Hero, Mapbox }
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,9 +4,7 @@ import {
 } from "@chakra-ui/react"
 import React from "react";
 import Container from "../components/layout/container"
-import BubbleGraph from "../components/bubblegraph/bubblegraph"
-// import Mapbox from "../components/mapbox"
-import PoliticsGraph from "../components/twitter/politicsgraph"
+import { BubbleGraph, PoliticsGraph } from "../components/index";
 
 export default function Home() {
  return (
