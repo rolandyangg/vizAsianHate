92a401e0938ebc04bfa92d11194582750da91794
whyroland <42717361+whyroland@users.noreply.github.com>
2021-08-01 12:46:52 -0400

    Last Formats

--- src/components/mapbox/mapbox.jsx
+++ src/components/mapbox/mapbox.jsx
@@ -16,6 +16,7 @@ import {
 	ModalBody,
 	ModalCloseButton,
 	useDisclosure,
+	Box
 } from '@chakra-ui/react';
 import { aapiHateCrimes } from './aapi-hate-crimes';
 import 'mapbox-gl/dist/mapbox-gl.css';
@@ -35,76 +36,85 @@ const Mapbox = () => {
 	const indexToYear = [0, 2021, 2020, 2019, 2018, 2017, 2016, 2015, 2006];
 
 	return (
-		<Tabs onChange={(index) => setYear(indexToYear[index])}>
-			<TabList height="5vh" color="white">
-				<Tab color="white">All Years</Tab>
-				<Tab color="white">2021</Tab>
-				<Tab color="white">2020</Tab>
-				<Tab color="white">2019</Tab>
-				<Tab color="white">2018</Tab>
-				<Tab color="white">2017</Tab>
-				<Tab color="white">2016</Tab>
-				<Tab color="white">2015</Tab>
-				<Tab color="white">2006</Tab>
-			</TabList>
-			<Map
-				style="mapbox://styles/mapbox/dark-v10"
-				containerStyle={{
-					height: '90vh',
-					width: '80vw',
-				}}
-				center={[-98.5795, 39.8283]}
-				zoom={[4.2]}
-				maxBounds={[
-					[-155.704594, 7.182507],
-					[-30.050213, 53.788833],
-				]}
-			>
-				{aapiHateCrimes
-					.filter((feature) => feature.properties.year === year || year === 0)
-					.map((feature, i) => {
-						return (
-							<Marker
-								key={i}
-								coordinates={feature.geometry.coordinates}
-								anchor="bottom"
-								onClick={() => {
-									setPlace(feature.properties.place_name);
-									setText(feature.properties.text);
-									setSource(feature.properties.source);
-									onOpen();
-								}}
-							>
-								{/* <Circle size="15px" bg="blue" color="#00FFFF" borderColor="white" borderWidth="1px" zIndex={0}/> */}
-								<Image
-									width="15px"
-									objectFit="cover"
-									alt="Marker"
-									src="images/marker.png"
-								/>
-							</Marker>
-						);
-					})}
-				<ZoomControl />
-			</Map>
-			<Modal isOpen={isOpen} onClose={onClose}>
-				<ModalOverlay />
-				<ModalContent>
-					<ModalHeader color="black">{place}</ModalHeader>
-					<ModalCloseButton />
-					<ModalBody>
-						<Text color="black">{text}</Text>
-					</ModalBody>
-					<ModalFooter>
-						<Link href={source} isExternal>
-							<Button color="black" colorScheme="yellow" mr={3}>
-								Read More
-							</Button>
-						</Link>
-					</ModalFooter>
-				</ModalContent>
-			</Modal>
-		</Tabs>
+		<Box>
+			<Tabs onChange={(index) => setYear(indexToYear[index])}>
+				<TabList height="5vh" color="white">
+					<Tab color="white">All Years</Tab>
+					<Tab color="white">2021</Tab>
+					<Tab color="white">2020</Tab>
+					<Tab color="white">2019</Tab>
+					<Tab color="white">2018</Tab>
+					<Tab color="white">2017</Tab>
+					<Tab color="white">2016</Tab>
+					<Tab color="white">2015</Tab>
+					<Tab color="white">2006</Tab>
+				</TabList>
+				<Map
+					style="mapbox://styles/mapbox/dark-v10"
+					containerStyle={{
+						height: '90vh',
+						width: '80vw',
+					}}
+					center={[-98.5795, 39.8283]}
+					zoom={[4.2]}
+					maxBounds={[
+						[-155.704594, 7.182507],
+						[-30.050213, 53.788833],
+					]}
+				>
+					{aapiHateCrimes
+						.filter((feature) => feature.properties.year === year || year === 0)
+						.map((feature, i) => {
+							return (
+								<Marker
+									key={i}
+									coordinates={feature.geometry.coordinates}
+									anchor="bottom"
+									onClick={() => {
+										setPlace(feature.properties.place_name);
+										setText(feature.properties.text);
+										setSource(feature.properties.source);
+										onOpen();
+									}}
+								>
+									{/* <Circle size="15px" bg="blue" color="#00FFFF" borderColor="white" borderWidth="1px" zIndex={0}/> */}
+									<Image
+										width="15px"
+										objectFit="cover"
+										alt="Marker"
+										src="images/marker.png"
+									/>
+								</Marker>
+							);
+						})}
+					<ZoomControl />
+				</Map>
+				<Modal isOpen={isOpen} onClose={onClose}>
+					<ModalOverlay />
+					<ModalContent>
+						<ModalHeader color="black">{place}</ModalHeader>
+						<ModalCloseButton />
+						<ModalBody>
+							<Text color="black">{text}</Text>
+						</ModalBody>
+						<ModalFooter>
+							<Link href={source} isExternal>
+								<Button color="black" colorScheme="yellow" mr={3}>
+									Read More
+								</Button>
+							</Link>
+						</ModalFooter>
+					</ModalContent>
+				</Modal>
+			</Tabs>
+			<Text>This interactive map shows location markers for reported Asian
+									Hate attacks in the past 15 years. You can choose which year you
+									want to view and can also view short descriptions and news
+									articles on the specific hate crimes. The purpose of this map is
+									to give insight into how drastically Asian Hate crimes have
+									increased over the past few years and a broader understanding of
+									how widespread Asian hate crimes are around the United States.</Text>
+		</Box>
 	);
 };
 
